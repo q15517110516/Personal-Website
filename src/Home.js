240cf7c80047ca20c85e578625cef1e08cfd17a7
q15517110516 <lmrzds@gmail.com>
2020-05-01 15:04:40 -0400

    projects complete

--- src/Home.js
+++ src/Home.js
@@ -95,7 +95,6 @@ export class Home extends Component {
                         <ol className="carousel-indicators">
                             <li data-target="#projects" data-slide-to="0"></li>
                             <li data-target="#projects" data-slide-to="1"></li>
-                            <li data-target="#projects" data-slide-to="2"></li>
                         </ol>
                         <div className="carousel-inner">
                             <div className="carousel-item active">
@@ -112,10 +111,16 @@ export class Home extends Component {
                                     <p style={{color: "black"}}>Nulla vitae elit libero, a pharetra augue mollis interdum</p>
                                 </div>
                             </div>
-                        
                         </div>
+                        <a className="carousel-control-prev" href="#projects" role="button" data-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#projects" role="button" data-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="sr-only">Next</span>
+                        </a>
                     </div>
-                    
                 </div>
                 <div className="home-footer"></div>
             </div>
